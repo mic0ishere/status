34c0f90d75c772d657003700484b50d5ce406680
mic0 <59290502+mic0ishere@users.noreply.github.com>
2023-12-26 18:18:08 +0100

    fix text alignment on mobile

--- components/AllWrapper.js
+++ components/AllWrapper.js
@@ -5,7 +5,7 @@ function AllWrapper({ children, className }) {
   return (
     <Card className="py-10 px-7 sm:py-14 md:py-20 md:px-10 mt-12 mb-20">
       <h1
-        className={`inline-flex justify-center w-full text-3xl md:text-4xl lg:text-5xl font-semibold ${className}`}
+        className={`inline-flex justify-center items-center w-full text-center text-3xl md:text-4xl lg:text-5xl font-semibold ${className}`}
       >
         <Circle animate />
         <span>{children}</span>
